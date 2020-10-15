7c2179bc6a6237e32f54990736a4d7ab511d374b
Carlos Soto Pérez <carlos348@outlook.com>
2020-10-14 20:10:15 -0500

    Contexto de ejecucióny líite de pools. Primer paso a integrar JSDocs.

---
+++ db/executionContext.js
@@ -0,0 +1,53 @@
+/**
+ * Módulo para ejecutar las consutltas a la base de datos bajo un mismo contexto.
+ * @author Carlos Soto Pérez <carlos348@outlook.com>
+ */
+
+const { getConnection } = require('../config/dbConfig');
+
+/**
+ * Definición de tipo de pool context
+ * @exports
+ */
+class PoolContext {
+  /**
+   * Constructor
+   * @param {import('mysql').PoolConnection} connection Connection pool
+   */
+  constructor(connection) {
+    this.connection = connection;
+  }
+}
+
+/**
+ * Definición de tipo del callback para la ejecución del contexto.
+ * @callback PoolContextCallback
+ * @param {PoolContext} context Context to execute with
+ * @returns {void} Promesa a ser ejecutada
+ */
+
+/**
+ * Ejecuta una función con un contexto
+ * @async
+ * @exports
+ * @throws {import('mysql').MysqlError}
+ * @param {PoolContextCallback} callback Callback a ejecutar con un contexto
+ */
+async function executionContext(callback) {
+  const connection = await getConnection();
+  let exception = null;
+  const context = new PoolContext(connection);
+  try {
+    callback(context);
+  } catch (e) {
+    exception = e;
+  }
+  connection.release();
+  if (exception) {
+    throw exception;
+  }
+}
+
+module.exports = {
+  executionContext,
+};
